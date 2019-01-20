ab7dc6132e17a8a613e5294bf6213659195c73e5
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2019-01-20 12:52:37 +0100

    Checkpoint

--- include/Shop.h
+++ include/Shop.h
@@ -5,8 +5,30 @@
 #ifndef PROJECT_SHOP_H
 #define PROJECT_SHOP_H
 
+#include <Product.h>
+#include <ShopAssistant.h>
+#include <memory>
+#include <vector>
 
 class Shop {
+private:
+    string shopName;
+    bool isOpen;
+    double budget;
+    vector<shared_ptr<Product>> products;
+    vector<shared_ptr<ShopAssistant>> employees;
+
+public:
+    Shop(string shopName);
+    ShopAssistant& getEmployee(int id);
+    bool checkIfOpen();
+    void changeShopState();
+    double getBudget();
+    void addMoney(double money);
+    double withdrawMoney(double money);
+    Product getProduct
+
+
 
 };
 
