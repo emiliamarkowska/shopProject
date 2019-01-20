e51d9c016cec97c7d090bf63ede8d4d3fe074710
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2019-01-20 11:11:25 +0100

    a

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Emila/Desktop/ShopProject/shopProject
-# Build directory: /mnt/c/Users/Emila/Desktop/ShopProject/shopProject/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test "/mnt/c/Users/Emila/Desktop/ShopProject/shopProject/cmake-build-debug/TestProject" "--report_level=detailed" "--log_level=all" "--color_output=yes")
